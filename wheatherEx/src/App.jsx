2300ffd5fa9e1e3e04c1e1bd7e5865946c8111dc
chickencoc <solutw@gmail.com>
2023-06-20 16:05:02 +0900

    weahter

--- wheatherEx/src/App.jsx
+++ wheatherEx/src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState("seoul");
+  const [weather, setWeather] = useState(null);
+
+  function fetchWeather() {
+    const apiKey = "1b9de2f304a91d21750ebcee97b6ca7d";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+  fetchWeather();
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="top">
+        <h1>" Применение погоды "</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="mid">
+        <div id="search">
+          <label for="abc">* Площадь :</label><input type="text" id="abc" placeholder="City Name"></input>
+        </div>
+        <div id="result">
+          <p>{"[ result ]"}</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
